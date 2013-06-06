6f597af8c72b9483f6ad6c3f530a17f002d449f7
luciano soro <soro.luc@gmail.com>
2013-06-05 22:19:27 -0300

    6ta subida

--- src/main/java/edu/tallerweb/cuentas/CuentaSueldo.java
+++ src/main/java/edu/tallerweb/cuentas/CuentaSueldo.java
@@ -1,54 +1,51 @@
 package edu.tallerweb.cuentas;
 
-/**
- * Es el tipo de cuenta más simple, ya que se rige por la premisa
- * de que en tanto y en cuanto se tenga tanto o más dinero en
- * cuenta del que se quiere extraer, la operación se debe efectuar
- * correctamente.
- */
 public class CuentaSueldo {
 
-	/**
-	 * No hay reglas adicionales para el depósito
-	 * @param monto a depositar
-	 */
-	public Double monto;
+	public Double monto=0.0;
 
-	
-	public void depositar(final Double monto) {
-		/*throw new RuntimeException("No implementado aún");*/
-		
-		
+	public void CuentaSueldo(final Double monto)
+	{
 		this.monto=monto;
 		
 		
+		
 	}
-
-	/**
-	 * No hay reglas adicionales para la extracción
-	 * @param monto a extraer
-	 */
-	public void extraer(final Double monto) {
-
-		if (this.monto < monto) {
-			throw new CuentaBancariaException("Algo");
+	
+	
+	public void depositar(final Double monto) {
+		
+		if(monto<0)
+		{
+			throw new CuentaBancariaException("No se puede depositar saldo negativo");
+			
 			
 		}
 		
 		
 		
-	this.monto=this.monto-monto;
+		this.monto=this.monto+monto;
+		
+		
+	}
+
 	
+	public void extraer(final Double monto) {
+
+		if (this.monto < monto) {
+			throw new CuentaBancariaException("No se puede extraer saldo negativo");
+			
+		}
+		else
+		{
+			this.monto=this.monto-monto;
+		}
 	
 	
 	}
 
-	/**
-	 * Permite saber el saldo de la cuenta
-	 * @return el saldo de la cuenta
-	 */
+
 	public Double getSaldo() {
-	//	throw new RuntimeException("No implementado aún");
 	
 	return monto;
 	
